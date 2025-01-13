a04a86c26fc13e1c8ace2ad632b26a7ae2e5f80f
monx.voll <stedy3348112688@gmail.com>
2025-01-13 16:19:59 -0500

    Controlador de borrado

--- src/main/java/controller/note/DeleteController.java
+++ src/main/java/controller/note/DeleteController.java
@@ -1,4 +1,18 @@
 package controller.note;
 
+import model.entities.User;
+import model.noteLogic.Delete;
+
+import java.util.Scanner;
+
 public class DeleteController {
+    private Delete delete;
+
+    public DeleteController(){
+        this.delete = new Delete();
+    }
+
+    public void deleteNote(User user, Scanner scanner) {
+     delete.removeNoteByName(user,scanner);
+    }
 }
