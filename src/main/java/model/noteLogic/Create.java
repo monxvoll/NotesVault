1a62e1126ce1b653cad8cb4f7cf30604df99776a
monx.voll <stedy3348112688@gmail.com>
2025-01-14 17:29:49 -0500

    Refactorizacion y manejo errores

--- src/main/java/model/noteLogic/Create.java
+++ src/main/java/model/noteLogic/Create.java
@@ -8,34 +8,36 @@ import model.entities.User;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.Executors;
 import java.util.UUID;
 import java.util.Scanner;
 
 
 public class Create {
 
-        private LocalDateTime localDateTime;
-        private String exclusiveId;
-        private Firestore firestore;
+    private LocalDateTime localDateTime;
+    private String exclusiveId;
+    private Firestore firestore;
 
-        public Create(){
-            this.localDateTime = LocalDateTime.now();
-            this.exclusiveId = UUID.randomUUID().toString();
-            this.firestore = FirestoreClient.getFirestore();
-        }
+    public Create(){
+        this.localDateTime = LocalDateTime.now();
+        this.exclusiveId = UUID.randomUUID().toString(); //Genera un ID unico utilizando UUID y lo convierte a String
+        this.firestore = FirestoreClient.getFirestore();
+    }
 
-        public void createNote(User user,Scanner scanner){
-            System.out.println("Digite el titulo de la nota");
-            String title = scanner.nextLine();
-            System.out.println("Digite el contenido de la nota");
-            String content = scanner.nextLine();
+    public void createNote(User user,Scanner scanner){
+        System.out.println("Digite el titulo de la nota");
+        String title = scanner.nextLine();
+        System.out.println("Digite el contenido de la nota");
+        String content = scanner.nextLine();
+
+        if(!checkIsNull(title, content)) {
             DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
             String date = localDateTime.format(format);
             System.out.println(exclusiveId);
-            Note note = new Note(title,content,date,exclusiveId);
-            addNote(user,note);
+            Note note = new Note(title, content, date, exclusiveId);
+            addNote(user, note);
         }
+    }
 
     private void addNote(User user, Note note) {
         try {
@@ -50,20 +52,22 @@ public class Create {
                 // Añadir la nueva nota a la lista de notas del usuario
                 ApiFuture<WriteResult> future = notesRef.document(note.getId()).set(note);
 
-                // Manejar el resultado de la operación asincrónica
-                future.addListener(() -> {
-                    try {
-                        WriteResult result = future.get(); // Se guarda el resultado de la operación
-                        System.out.println("Nota guardada exitosamente. Hora: " + result.getUpdateTime());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }, Executors.newSingleThreadExecutor()); // Crear un hilo encargado de ejecutar el listener
+                //Obtener el resultado de la operacion asincrona
+                WriteResult result = future.get();
+                System.err.println("Nota guardada exitosamente."+result.getUpdateTime());
             } else {
                 System.out.println("Error: Usuario o nota no válidos.");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    static boolean checkIsNull(String title,String content) {
+        if(title.isEmpty() || content.isEmpty()){
+            System.err.println("Por favor digite un campo valido");
+            return true;
+        }
+        return false;
     }
 }
