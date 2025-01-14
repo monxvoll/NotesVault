0f0adb79646e731ec355b282ec53b2f0c4091683
monx.voll <stedy3348112688@gmail.com>
2025-01-14 17:29:35 -0500

    Se culmina el metodo de edicion de notas junto a la refactorizacion y manejo de errores

--- src/main/java/controller/note/UpdateControlller.java
+++ src/main/java/controller/note/UpdateControlller.java
@@ -1,4 +1,16 @@
 package controller.note;
 
+import model.entities.User;
+import model.noteLogic.Update;
+
+import java.util.Scanner;
+
 public class UpdateControlller {
+    private Update update;
+    public UpdateControlller(){
+      this.update = new Update();
+    }
+     public void updateNote(User user, Scanner scanner){
+         update.updateNote(user,scanner);
+     }
 }
