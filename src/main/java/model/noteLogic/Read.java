1c7d0de87f0a79f02a008474996055f9b8ec6560
monx.voll <stedy3348112688@gmail.com>
2025-01-14 17:30:31 -0500

    Refactorizacion y manejo errores

--- src/main/java/model/noteLogic/Read.java
+++ src/main/java/model/noteLogic/Read.java
@@ -7,17 +7,53 @@ import model.entities.Note;
 import model.entities.User;
 
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Read {
-    private Firestore firestore;
+    private static Firestore firestore;
 
-    public Read(){
-        this.firestore  = FirestoreClient.getFirestore();
+    static {
+        firestore = FirestoreClient.getFirestore();
     }
 
     public void readNotes(User user) {
         try {
+            List<QueryDocumentSnapshot> documents = getUserNotesCollection(user);
+
+            if(documents==null){
+                System.err.println("Error al traer las notas del usuario");
+            }else {
+                if (hasNotes(documents)) {
+                    for (QueryDocumentSnapshot document : documents) {
+                        Note note = document.toObject(Note.class);
+                        System.out.println(note);
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    static void enumerateNotes(List<QueryDocumentSnapshot> documents){
+        for (int i = 1; i <=documents.size() ; i++) {
+            Note note = documents.get(i - 1).toObject(Note.class);
+            System.out.println(i + ". " + note);
+        }
+    }
+
+    static boolean hasNotes(List<QueryDocumentSnapshot> documents){
+        if(documents.isEmpty()){
+            System.err.println("Usted actualmente no tiene notas");
+            return false;
+        }
+        return true;
+    }
+
+     static List<QueryDocumentSnapshot> getUserNotesCollection(User user){
+        try {
+            // Referencia al documento del usuario actual según su nombre de usuario
             DocumentReference userRef = firestore.collection("users").document(user.getUserName());
             //Se usa ApiFuture, que es clase que representa un resultado que estará disponible en el futuro.
             //Y QuerySnapshot, seria el contenedor de los resultados de busqueda. Es decir una lista de documentos que cumplen los requisitos de busqueda
@@ -25,13 +61,25 @@ public class Read {
 
             List<QueryDocumentSnapshot> documents = future.get().getDocuments();
 
-            for (QueryDocumentSnapshot document : documents){
-                Note note = document.toObject(Note.class); //Convertir el documento a un objeto
-                System.out.println(note);
-            }
-
+            return documents;
         }catch (Exception e){
             e.printStackTrace();
         }
+         System.out.println("aaa");
+        return null;
+
+     }
+
+     static int getNoteIndex(Scanner scanner, List<QueryDocumentSnapshot> documents ){
+
+        System.out.println("Por favor ingrese el número de la nota que desea actualizar : ");
+        int noteIndex = scanner.nextInt();
+        scanner.nextLine();
+
+        if (noteIndex < 1 || noteIndex > documents.size()) {
+            System.err.println("Por favor ingrese un número válido");
+            return -1;
+        }
+        return noteIndex-1;
     }
 }
