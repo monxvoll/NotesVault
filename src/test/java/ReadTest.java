6083b242a58d98a88621f06ceb3caee52a78d537
monx.voll <stedy3348112688@gmail.com>
2025-01-25 15:00:33 -0500

    Paquete para pruebas

---
+++ src/test/java/ReadTest.java
@@ -0,0 +1,43 @@
+
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import model.entities.User;
+import model.noteLogic.Read;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import view.FirebaseInitializer;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+public class ReadTest {
+    private static User user;
+
+    @BeforeAll
+    public static void initialize(){
+        FirebaseInitializer.initialize(); //inicializa firebase una sola vez ants de cada prueba
+        user = new User("OrionRat", "OrionRat123@");
+    }
+
+    @Test
+    public void testGetUserNotesCollection(){
+        List<QueryDocumentSnapshot> documents = Read.getUserNotesCollection(user);
+        assertNotNull(documents,"La coleccion de notas no debe ser null");
+    }
+
+    @Test
+    public void testReadNotes(){
+        Read read = new Read();
+        read.readNotes(user);
+        //salida en consola
+    }
+
+    @Test
+    public void testHasNotes(){
+        List<QueryDocumentSnapshot> emptyList = List.of(); //lista vacia
+        assertFalse(Read.hasNotes(emptyList),"El usuario no deberia tener notas");
+        QueryDocumentSnapshot mockDocument = mock(QueryDocumentSnapshot.class); //se usa mockito para crear un objeto simulado "mockDocument"
+        List<QueryDocumentSnapshot> list = List.of(mockDocument); //lista que contiene notas simuladas
+        assertTrue(Read.hasNotes(list),"El usuario deberia tener notas");
+    }
+
+}
