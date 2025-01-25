bc56daf5a87dc5e7605c211950b90ac5e9572bc1
monx.voll <stedy3348112688@gmail.com>
2025-01-25 15:54:43 -0500

    Se añade la injeccion de dependencias faltante

--- src/main/java/controller/auth/LoginController.java
+++ src/main/java/controller/auth/LoginController.java
@@ -1,13 +1,19 @@
 package controller.auth;
 
+import com.google.cloud.firestore.Firestore;
 import model.authlogic.Login;
 import model.entities.User;
+import util.FirestoreInitializer;
+
+import java.util.Scanner;
 
 public class LoginController {
         private Login login ;
 
         public LoginController() {
-           this.login = new Login();
+            Scanner scanner = new Scanner(System.in);
+            Firestore firestore = FirestoreInitializer.getFirestore();
+            this.login = new Login(scanner,firestore);
         }
 
         public boolean login() {
