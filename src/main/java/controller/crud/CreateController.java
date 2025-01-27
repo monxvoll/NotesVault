57cdb0f21cc4e1d2e74cfc41168fc9c437ab9901
monx.voll <stedy3348112688@gmail.com>
2025-01-27 17:02:09 -0500

    Añadida abstraccion de inputprovider para testing

--- src/main/java/controller/crud/CreateController.java
+++ src/main/java/controller/crud/CreateController.java
@@ -4,6 +4,7 @@ import com.google.cloud.firestore.Firestore;
 import model.entities.User;
 import model.crudLogic.Create;
 import util.FirestoreInitializer;
+import util.InputProvider;
 
 import java.util.Scanner;
 
@@ -11,12 +12,14 @@ public class CreateController {
     private Create create;
 
     public CreateController(){
+        Scanner scanner = new Scanner(System.in);
+        InputProvider inputProvider = new InputProvider(scanner);
         Firestore firestore = FirestoreInitializer.getFirestore();
-        this.create = new Create(firestore);
+        this.create = new Create(inputProvider,firestore);
     }
 
-    public void createNote(User user, Scanner scanner){
-        create.createNote(user,scanner);
+    public void createNote(User user){
+        create.createNote(user);
     }
 
 }
