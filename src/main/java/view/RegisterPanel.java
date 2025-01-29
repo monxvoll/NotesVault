ad108942dba676b370994f03986ea371a112793b
JoseSLK <jose.salamanca05@uptc.edu.co>
2025-01-29 12:50:30 -0500

    Estructura grafica con swing -- Login check

---
+++ src/main/java/view/RegisterPanel.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class RegisterPanel extends JPanel {
+    private MainApp mainApp;
+    RegisterPanel(MainApp mainApp){
+        this.mainApp = mainApp;
+    }
+}
