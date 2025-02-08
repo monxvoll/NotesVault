69195d073297d1e2f8ea25b73b567ebc60cc9c0c
monx.voll <stedy3348112688@gmail.com>
2025-02-08 15:51:17 -0500

    Se cambia el manejo, por variables de entorno

--- src/main/java/util/FirebaseInitializer.java
+++ src/main/java/util/FirebaseInitializer.java
@@ -10,20 +10,32 @@ public class FirebaseInitializer {
 
     // Método para inicializar Firebase
     public static void initialize() {
+
         try {
-            // Crea un flujo de entrada para leer el archivo de claves JSON
-            FileInputStream serviceAccount = new FileInputStream("resources/key.json"); //Ruta al json con las credenciales
+            // Obtener la ruta del archivo JSON desde la variable de entorno
+            String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
 
-            // Construye las opciones de Firebase con las credenciales obtenidas del archivo JSON y la URL de la base de datos
+            if (credentialsPath == null || credentialsPath.isEmpty()) {
+                throw new RuntimeException("No se encontró la variable de entorno GOOGLE_APPLICATION_CREDENTIALS");
+            }
+
+            // Leer el archivo JSON y obtener las credenciales
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+
+            // Construye las opciones de Firebase con las credenciales obtenidas y la URL de la base de datos
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount)) // Establece las credenciales
-                    .setDatabaseUrl("https://todotaskapp-d2cf1.firebaseio.com") // Establece la URL de la base de datos
+                    .setCredentials(credentials) // Establece las credenciales
+                    .setDatabaseUrl("https://todotaskapp-d2cf1.firebaseio.com") // URL de la base de datos
                     .build();
+            // Inicializa la app de Firebase si aún no está inicializada
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                System.out.println("Firebase inicializado correctamente!");
+            } else {
+                System.out.println("Firebase ya estaba inicializado.");
+            }
 
-            // Inicializa la app de Firebase con las opciones especificadas
-            FirebaseApp.initializeApp(options);
         } catch (IOException e) {
-            // Maneja cualquier excepción de entrada/salida que pueda ocurrir
             e.printStackTrace();
         }
     }
