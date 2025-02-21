a8c4633f7eee288367fa2169df398eef3501750b
monx.voll <stedy3348112688@gmail.com>
2025-02-20 20:15:52 -0500

    Se añade un record para definir lo que se expondra en la api

---
+++ src/main/java/model/entities/UserDTO.java
@@ -0,0 +1,3 @@
+package model.entities;
+//Record creado para definir lo que se expondra en la api , un record automaticamente genera metodos getter, to string etc..
+public record UserDTO(String email, String userName) {}
