b1928fc0b53334fdc7cffd69614380d587e8d9d1
monx.voll <stedy3348112688@gmail.com>
2025-03-11 21:08:19 -0500

    Se cambian los RequestParams a RequestBody, para recibir el objeto directamente

--- src/main/java/controller/auth/LoginController.java
+++ src/main/java/controller/auth/LoginController.java
@@ -1,10 +1,12 @@
 package controller.auth;
 
 import model.authlogic.LoginService;
+import model.entities.User;
 import model.entities.UserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,14 +23,18 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestParam String username, @RequestParam String password) {
-        logger.info("Solicitud de inicio de sesión recibida para usuario: {}", username);
+    public ResponseEntity<?> loginUser(@RequestBody User user) {
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("El cuerpo de la solicitud no puede estar vacio");
+        }
+        logger.info("Solicitud de inicio de sesión recibida para usuario: {}", user.getUserName());
         try {
-            UserDTO loggedUser = loginService.loginUser(username, password);
-            logger.info("Inicio de sesión exitoso para usuario: {}", username);
+            UserDTO loggedUser = loginService.loginUser(user);
+            logger.info("Inicio de sesión exitoso para usuario: {}", loggedUser);
             return ResponseEntity.ok(loggedUser); // Retorna el usuario (solo su nombre y email)
         } catch (IllegalArgumentException e) {
-            logger.warn("Error en el inicio de sesión para usuario {}: {}", username, e.getMessage());
+            logger.warn("Error en el inicio de sesión para usuario {}: {}", user.getUserName(), e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
