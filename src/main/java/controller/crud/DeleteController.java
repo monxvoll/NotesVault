aea975567ce34e18364be9085750b5719dd959a0
monx.voll <stedy3348112688@gmail.com>
2025-03-22 18:16:03 -0500

    Adaptacion  controlador a arquitectura RESTful en Spring Boot finalizada

--- src/main/java/controller/crud/DeleteController.java
+++ src/main/java/controller/crud/DeleteController.java
@@ -1,20 +1,35 @@
 package controller.crud;
 
+
+import model.crudLogic.DeleteService;
+import model.entities.Note;
 import model.entities.User;
-import model.crudLogic.Delete;
-import util.InputProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Scanner;
-
+@RestController
+@RequestMapping("/note")
 public class DeleteController {
-    private Delete delete;
+    private static final Logger logger = LoggerFactory.getLogger(CreateController.class);
+    private final DeleteService deleteService;
 
-    public DeleteController(){
-        this.delete = new Delete();
+    public DeleteController(DeleteService deleteService){
+        this.deleteService = deleteService;
     }
 
-    public void deleteNote(User user, Scanner scanner) {
-        InputProvider inputProvider = new InputProvider(scanner);
-        delete.removeNoteByName(user,inputProvider);
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteNote(@RequestParam String userEmail,@RequestParam  String noteId) {
+        logger.info("Solicitud de eliminacion para nota con ID: {}",noteId);
+        try{
+            deleteService.deleteNote(userEmail,noteId);
+            logger.info("Solicitud de eliminacion exitosa para usuario: {}", userEmail);
+            return ResponseEntity.ok("Nota eliminada exitosamente");
+        } catch (ResponseStatusException e) {
+            logger.error("Error en la eliminacion de la nota: {}", e.getMessage());
+            return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+        }
     }
 }
