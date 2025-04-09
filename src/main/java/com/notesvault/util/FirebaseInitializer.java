cd5887fb892798c87f078040a99ae8419a6131c6
monx.voll <stedy3348112688@gmail.com>
2025-04-09 17:04:55 -0500

    Se elimina la clave como variable de entorno, ahora se usa con su path

--- src/main/java/com/notesvault/util/FirebaseInitializer.java
+++ src/main/java/com/notesvault/util/FirebaseInitializer.java
@@ -5,29 +5,28 @@ import com.google.firebase.FirebaseOptions;
 import com.google.auth.oauth2.GoogleCredentials;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class FirebaseInitializer {
 
-    // Método para inicializar Firebase
     public static void initialize() {
-
         try {
-            // Obtener la ruta del archivo JSON desde la variable de entorno
-            String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+           
+            InputStream serviceAccount = FirebaseInitializer.class
+                    .getClassLoader()
+                    .getResourceAsStream("firebase/key.json");
 
-            if (credentialsPath == null || credentialsPath.isEmpty()) {
-                throw new RuntimeException("No se encontró la variable de entorno GOOGLE_APPLICATION_CREDENTIALS");
+            if (serviceAccount == null) {
+                throw new RuntimeException("No se encontró el archivo de credenciales en resources/firebase/key.json");
             }
 
-            // Leer el archivo JSON y obtener las credenciales
-            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
 
-            // Construye las opciones de Firebase con las credenciales obtenidas y la URL de la base de datos
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(credentials) // Establece las credenciales
-                    .setDatabaseUrl("https://todotaskapp-d2cf1.firebaseio.com") // URL de la base de datos
+                    .setCredentials(credentials)
+                    .setDatabaseUrl("https://todotaskapp-d2cf1.firebaseio.com")
                     .build();
-            // Inicializa la app de Firebase si aún no está inicializada
+
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
                 System.out.println("Firebase inicializado correctamente!");
@@ -39,4 +38,5 @@ public class FirebaseInitializer {
             e.printStackTrace();
         }
     }
+
 }
