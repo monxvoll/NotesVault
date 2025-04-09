1caaf7cc40c6fb25e50cff39d6a7201780935332
monx.voll <stedy3348112688@gmail.com>
2025-04-09 17:33:50 -0500

    Elimina archivos innecesarios y ajusta FirebaseInitializer para ser reconocida por Spring Boot

--- src/main/java/com/notesvault/util/FirebaseInitializer.java
+++ src/main/java/com/notesvault/util/FirebaseInitializer.java
@@ -1,22 +1,25 @@
 package com.notesvault.util;
-
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.auth.oauth2.GoogleCredentials;
-import java.io.IOException;
-import java.io.InputStream;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.io.FileInputStream;
 
+
+@Configuration //Le decimos a spring que esta clase es una fuente de beans (Un bean es un objeto listo para ser injectado)
 public class FirebaseInitializer {
 
-    public static void initialize() {
+    @Bean //Se añade esta anotacion para que springboot puede hacer la injeccion de dependencias de firestore de forma automatica
+    public Firestore firestore() {
         try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-config.json");
 
-            InputStream serviceAccount = FirebaseInitializer.class
-                    .getClassLoader()
-                    .getResourceAsStream("resources/key.json");
 
             if (serviceAccount == null) {
-                throw new RuntimeException("No se encontró el archivo de credenciales en resources/firebase/key.json");
+                throw new RuntimeException("No se encontró el archivo de credenciales en resources/firebase-config.json");
             }
 
             GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
@@ -33,8 +36,10 @@ public class FirebaseInitializer {
                 System.out.println("Firebase ya estaba inicializado.");
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            return FirestoreClient.getFirestore();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error al inicializar Firebase", e);
         }
     }
 
