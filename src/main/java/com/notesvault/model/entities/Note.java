5351d879f08d18d7572d28238ecdd01969f62079
monx.voll <stedy3348112688@gmail.com>
2025-05-24 14:55:11 -0500

    Add isActive field to Note model

--- src/main/java/com/notesvault/model/entities/Note.java
+++ src/main/java/com/notesvault/model/entities/Note.java
@@ -7,7 +7,7 @@ public class Note {
     private String date;
     private String content;
     private String id;
-
+    private boolean isActive; // Nuevo campo
 
     /*Constructor vacío para firestore porque, al deserializar un documento (convertirlo en un objeto Java)
     , crea una instancia vacía de la clase y luego llena sus atributos con los datos del documento.
@@ -16,11 +16,12 @@ public class Note {
 
     }
 
-    public Note(String title, String content, String date, String id) {
+    public Note(String title, String content, String date, String id, boolean isActive) {
         this.title = title;
         this.id = id;
         this.date = date;
         this.content = content;
+        this.isActive = isActive;
     }
 
     public String getTitle() {
@@ -55,6 +56,14 @@ public class Note {
         this.id = id;
     }
 
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
     @Override
     public String toString() {
         return "Note{" +
@@ -62,6 +71,7 @@ public class Note {
                 ", date='" + date + '\'' +
                 ", content='" + content + '\'' +
                 ", id='" + id + '\'' +
+                ", isActive=" + isActive +
                 '}';
     }
 }
