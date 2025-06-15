6fd937842973cf5a0d34399e5e09414467308bc0
monx.voll <stedy3348112688@gmail.com>
2025-06-15 16:21:29 -0500

    Create ReadController with /read endpoint

---
+++ src/main/java/com/notesvault/controller/crud/UpdateController.java
@@ -0,0 +1,32 @@
+package com.notesvault.controller.crud;
+
+import com.notesvault.dtos.NoteDTO;
+import com.notesvault.model.crudLogic.UpdateService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/note")
+public class UpdateController {
+    private static final Logger logger = LoggerFactory.getLogger(UpdateController.class);
+    private UpdateService updateService;
+
+    public UpdateController(UpdateService updateService) {
+        this.updateService = updateService;
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<?> UpdateNote(@PathVariable String id, @RequestBody NoteDTO noteDTO){
+        try{
+            logger.info("Solicitud de actualizacion en la nota con id {} para el usuario {}", id, noteDTO.getUserEmail());
+            updateService.updateNote(id,noteDTO);
+            return ResponseEntity.ok("Nota Actualizada Correctamente");
+        }catch (ResponseStatusException e){
+            logger.error("Error en la actualizacion de la nota: {}", e.getMessage());
+            return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+        }
+    }
+}
