9b6eb3b5a2522fb913a44ddbd5a9c8f135e0807f
monxvll <stedy3348112688@gmail.com>
2026-01-12 18:56:58 -0500

    fix(security): enable CORS and permit access to error endpoint
    - Added '/error' to permitAll() to expose actual exception status codes.
    - Configured CORS to allow requests from Swagger UI and browsers.

--- src/main/java/com/notesvault/config/security/SecurityConfig.java
+++ src/main/java/com/notesvault/config/security/SecurityConfig.java
@@ -2,11 +2,17 @@ package com.notesvault.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 /**
  *This class configures the security filter chain and defines access rules
@@ -24,10 +30,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // 1. Disable CSRF (Unnecessary for stateless REST APIs using JWTs)
+                //Enable CORS
+                .cors(Customizer.withDefaults())
+
+                // 2. Disable CSRF (Unnecessary for stateless REST APIs using JWTs)
                 .csrf(AbstractHttpConfigurer::disable)
 
-                // 2. Configure access rules (Authorization)
+                // 3. Configure access rules (Authorization)
                 .authorizeHttpRequests(auth -> auth
                         // Public endpoints: Login, recovery, and documentation (No token required)
                         .requestMatchers(
@@ -36,7 +45,8 @@ public class SecurityConfig {
                                         "/account/delete-confirmation", // Allow email confirmation links (no Auth header required)
                                         "/account/resend-delete-confirmation",
                                         "/swagger-ui/**",
-                                        "/v3/api-docs/**"
+                                        "/v3/api-docs/**",
+                                        "/error" // added to see possible exceptions instead of 403
                                 ).permitAll()
                         // Private endpoints: All other requests require a valid token
                         .anyRequest().authenticated()
@@ -46,5 +56,19 @@ public class SecurityConfig {
                 .addFilterBefore(firebaseTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    //CORS to allow request from Swagger UI or others origins
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
